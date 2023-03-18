4b3e6a31135e82210d4eab221ad5980fac2adc7e
mayconSR <mayconsilvask@gmail.com>
2023-03-18 17:53:34 -0300

    corrigido a porta no arquivo index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${server.address().port}`);
 });
