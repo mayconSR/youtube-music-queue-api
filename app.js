22d8fcbec5ae560912d25e8d0adf7a0faba3bf6a
mayconSR <mayconsilvask@gmail.com>
2023-03-18 21:03:20 -0300

    atualizado para versão que estrai o ID do video para api do youtube

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const url = require('url');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const corsOptions = {
+  origin: 'https://youtube-music-queue-app.vercel.app',
+  optionsSuccessStatus: 200
+};
 
 let musicLinks = [];
 
@@ -13,9 +19,21 @@ app.get('/', (req, res) => {
 });
 
 // verificar se o link é do YouTube
-function isValidYouTubeLink(url) {
-  const regex = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
-  return url.match(regex);
+function isValidYouTubeLink(link) {
+  const { host } = url.parse(link);
+  return (host === 'www.youtube.com' || host === 'youtube.com' || host === 'youtu.be');
+}
+
+// Extrair o ID do vídeo do link do YouTube
+function getYouTubeVideoId(link) {
+  const { hostname, pathname, searchParams } = url.parse(link, true);
+  if (hostname === 'www.youtube.com' || hostname === 'youtube.com') {
+    return searchParams.get('v');
+  } else if (hostname === 'youtu.be') {
+    return pathname.substr(1);
+  } else {
+    return null;
+  }
 }
 
 // Rota para adicionar um link de música
@@ -30,7 +48,12 @@ app.post('/api/music', (req, res) => {
     return res.status(400).json({ error: 'O link fornecido não é um link válido do YouTube.' });
   }
 
-  musicLinks.push(link);
+  const videoId = getYouTubeVideoId(link);
+  if (!videoId) {
+    return res.status(400).json({ error: 'Não foi possível obter o ID do vídeo do link fornecido.' });
+  }
+
+  musicLinks.push(videoId);
   res.status(201).json({ message: 'Link de música adicionado com sucesso.' });
 });
 
